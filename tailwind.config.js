b630dbfbeda1c2d18d491b0a0ca1edc07a31a9cc
darek36 <dariusz.c27@gmail.com>
2022-11-18 22:09:02 +0100

    tailwind reparatur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./srcJsHtml/**/*.{html,js}",
+        // "./srcJsHtml/**/*.{html,js}",
+        "./srcJsHtml/**/*.js",
+        "./**/*.html",
     ],
     theme: {
         extend: {},
